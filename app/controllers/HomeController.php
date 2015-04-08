6b076adab4ecccfd280e43eda8ff416704995965
muhtarudinsiregar <redcar.studious@gmail.com>
2015-04-08 23:42:49 +0700

    fix query search

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -18,31 +18,58 @@ class HomeController extends BaseController {
 
 	public function index()
 	{
-		
-		return View::make('home/main')->withTitle('');
+		$data = DB::table('buku')->get();
+		$kategori = DB::table('kategori')->get();
+		$data = [
+		'data'=>$data,
+		'kategori'=>$kategori
+		];
+		// dd($kategori);
+		return View::make('home/main',$data)->withTitle('');
 
 	}
 
 	public function cari()
 	{
 		$keyword = Input::get('search');
+		$kategori = Input::get('kategori');
 
 		if (empty($keyword)) {
-			$data = Home::all();
-			$data = [
-				'data'=>$data
-			];
-			// return View::make('pencarian/index',$data);
+			$data = DB::table('buku')->get();
+			
+			
 		}else{
 
-			$data = Home::where('penulis','LIKE',$keyword)->get();
-			$data = [
-				'data'=>$data
-			];
-			
-		}
+			if ($kategori==0) {
+				// ketika kategori 0 atau semua kategori maka cari semua buku
+				$data = DB::table('buku')
+				->select('id','judul','harga','gambar','penulis')				
+				->where('judul', 'LIKE', '%'.$keyword.'%')
+				->orWhere('penulis','LIKE','%'.$keyword.'%')
+				->get();
 
-		return View::make('pencarian/index',$data)->withTitle('pencarian');
+			}
+			else{
+				// ketika kategori bukan 0 maka tampilkan judul, harga, penulis dan id dimana kategori = $kategori kemudian
+				// menggunakan subquery menjadi and (where judul like $keyword or penulis like $keyword
+				// sql select judul,harga,buku,gambar from buku where kategori_id=2 and (judul like '%radit%'  or penulis  like '%radit%')
+				
+				$data = DB::table('buku')
+				->select('id','judul','harga','penulis','gambar')
+				->where('kategori_id','=',$kategori)
+				->where(function($query){
+					$keyword = Input::get('search');
+					$query->where('judul', 'LIKE', '%'.$keyword.'%')
+						  ->orWhere('penulis','LIKE','%'.$keyword.'%');
+				})
+				->get();
+			}	
+		}
+		$data = [
+			'data'=>$data
+			];
+
+		return View::make('pencarian/index',$data)->withTitle('Pencarian');
 	} //end func cari()
 
 	public function keranjang()
